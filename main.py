59eee893c22be76e40a2b7c71f53f857c1c9a440
Rodarbal <91778305+Ducks-Scripts@users.noreply.github.com>
2022-05-14 04:22:02 +0000

    Changed from client to bot

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import discord, os, asyncio
 from discord.ext import commands
 from discord.ext.commands import Bot
 
+bot = commands.Bot(command_prefix='?')
 
-client = discord.Client()
-
-@client.event
+@bot.event
 async def on_ready():
-  print("logged in as {0.user}".format(client))
+  print("logged in as {0.user}".format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
   if message.content.startswith("Ping"):
     await message.channel.send("Pong")
+  
+
 
 token = os.environ['token']
-client.run(token)
+bot.run(token)
 
 # https://discord.com/api/oauth2/authorize?client_id=974273433846304768&permissions=2214841408&scope=bot
