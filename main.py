2a5a808c0f1603a453c983308de4211de0508ba3
Rodarbal <91778305+Ducks-Scripts@users.noreply.github.com>
2022-05-14 04:19:01 +0000

    Bot works now and has basic functionality

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import discord, os, asyncio
-from discord.ext.commands import Bot
 from discord.ext import commands
+from discord.ext.commands import Bot
 
 
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='$', intents=intents)
-print("Bot Online.")
+client = discord.Client()
 
-@bot.command()
+@client.event
+async def on_ready():
+  print("logged in as {0.user}".format(client))
+
+@client.event
 async def on_message(message):
-  if message.content.startswith('Ping'):
-    channel = message.channel
-    await channel.send("Pong")
+  if message.content.startswith("Ping"):
+    await message.channel.send("Pong")
 
 token = os.environ['token']
-bot.run(token)
+client.run(token)
 
 # https://discord.com/api/oauth2/authorize?client_id=974273433846304768&permissions=2214841408&scope=bot
