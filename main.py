8fc9e2361933179500121e5c985dc22166f34318
Rodarbal <91778305+Ducks-Scripts@users.noreply.github.com>
2022-05-14 09:51:37 +0000

    banning in progress

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import discord, os, asyncio
+import discord, os, asyncio, random
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-bot = commands.Bot(command_prefix='?')
+bot = commands.Bot(command_prefix = '.')
 
 @bot.event
 async def on_ready():
   print("Logged in as {0.user}".format(bot))
+  await bot.change_presence(status=discord.Status.dnd, activity=discord.Game("Duelist"))
+
+@bot.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+  await member.ban(reason=reason)
 
 @bot.event
 async def on_message(message):
@@ -16,12 +21,20 @@ async def on_message(message):
        await message.channel.send("Slay indeed father.")
     else:
       await message.channel.send("Slay indeed.")
-  if message.content.startswith("Hello"):
+  elif message.content.startswith("Hello"):
     await message.channel.send("Hi, {}.".format(username))
-  
 
 
-token = os.environ['token']
-bot.run(token)
+bot.run(os.environ['token'])
 
-# https://discord.com/api/oauth2/authorize?client_id=974273433846304768&permissions=2214841408&scope=bot
+
+
+
+#plan for bot
+#  - moderation
+#    - ban command
+#    - antiswear and warn
+#  - scrap github raw and format in announcing
+#    - scrape https://raw.githubusercontent.com/Ducks-Scripts/duelist-game/main/changelog.md?token=GHSAT0AAAAAABUKAM6BLCWQJAKG3GUA5MXMYT7NL6Q
+#    - send embed with update as title and changes as dot points
+# https://discord.com/api/oauth2/authorize?client_id=974273433846304768&permissions=8&scope=bot
